9450fe18a424b7f1d9310550303202435f9a6a45
MarcoAntonioGarcia <70038063+MarcoAntonioGarcia@users.noreply.github.com>
2023-03-16 14:48:29 -0600

    Add files via upload

---
+++ spring-boot-monolito/src/main/java/com/springboot/monolito/SpringBootMonolitoApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.monolito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMonolitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMonolitoApplication.class, args);
+	}
+
+}
