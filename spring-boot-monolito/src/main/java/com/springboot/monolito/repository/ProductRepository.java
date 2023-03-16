8a7714ea827b9c69b5aa8ab4486f88780f06fd23
MarcoAntonioGarcia <70038063+MarcoAntonioGarcia@users.noreply.github.com>
2023-03-16 14:46:53 -0600

    Add files via upload

---
+++ spring-boot-monolito/src/main/java/com/springboot/monolito/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.springboot.monolito.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springboot.monolito.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+	List<Product> findByBrand(String brand);
+	List<Product> findByPriceGreaterThan(float price);
+}
